32d4fb12dd16ee3ec5f07bf96d78bc9d95b859c4
f4354 <aysunkurban82@gmail.com>
2023-02-01 12:25:59 +0000

    added build scripts for QA Environment

--- jenkins/deploy_app_on_qa_environment.sh
+++ jenkins/deploy_app_on_qa_environment.sh
@@ -8,7 +8,7 @@ AWS_REGION=$AWS_REGION helm s3 push --force petclinic_chart-${BUILD_NUMBER}.tgz 
 kubectl create ns petclinic-qa || echo "namespace petclinic-qa already exists"
 kubectl delete secret regcred -n petclinic-qa || echo "there is no regcred secret in petclinic-qa namespace"
 kubectl create secret generic regcred -n petclinic-qa \
-    --from-file=.dockerconfigjson=/var/lib/jenkins/.docker/config.json \
+    --from-file=.dockerconfigjson=/var/lib/jenkins/.docker/config.json \    
     --type=kubernetes.io/dockerconfigjson
 AWS_REGION=$AWS_REGION helm repo update
 AWS_REGION=$AWS_REGION helm upgrade --install \
